c118cbdc0fca4d488d13159c6f0f4bfd94bb3af9
ShUmarbek <shumarbek001@gmail.com>
2026-05-21 16:33:37 +0500

    Router was updated and fixed by adding Outlet

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Login from "./pages/Login";
 import Header from "./components/Header";
 
 function App() {
@@ -11,8 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Header />}>
           <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 
 const Header = () => {
     return (
@@ -8,8 +8,10 @@ const Header = () => {
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
+                    <li><Link to="/login">Login</Link></li>
                 </ul>
             </nav>
+            <Outlet />
         </header>
     )
 }
